763ccabdc1e601ab986ca6f69e3257552fb7c122
Lorraineken <lorrainekupa@gmail.com>
2023-01-14 11:32:25 +0300

    filtered items by Category

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+ 
+  const [inCart,setCart]=useState(false)
+
+  const addCart = inCart ? "in-cart":"";
+  const itemCart = inCart ? "Remove From Cart":"Add to Cart"
+
+  function handleCart(){
+    setCart(!inCart)
+  }
+
+
   return (
-    <li className="">
+    <li className={addCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{itemCart}</button>
     </li>
   );
 }
